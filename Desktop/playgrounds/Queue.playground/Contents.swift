df6e4a51f2e245e424b140d4ca96232013dc4976
fiona <fi.carty@gmail.com>
2017-03-18 20:12:04 -0700

    cleaning up Queue file

--- Desktop/playgrounds/Queue.playground/Contents.swift
+++ Desktop/playgrounds/Queue.playground/Contents.swift
@@ -121,6 +121,8 @@ public struct BetterQueue<T> {
     }
 }
 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
 // MARK: testing it out
 
 var q = BetterQueue<String>()
