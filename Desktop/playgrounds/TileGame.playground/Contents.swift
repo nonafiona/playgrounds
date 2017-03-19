f35ef4fbe7b43fd4816b3a423820fa92453abe71
fiona <fi.carty@gmail.com>
2017-03-19 16:14:12 -0700

    me: work on your portfolio... also me: code a 2048 clone in playgrounds

---
+++ Desktop/playgrounds/TileGame.playground/Contents.swift
@@ -0,0 +1,31 @@
+// What are you making? 
+
+// A 2048 clone that's probably cooler. 
+// ( but everyone says that 😏 ) 
+
+// MARK: Movement orientation 
+// TODO: enum with vertical & horizontal cases
+
+// MARK: Movement direction 
+// TODO: enum with backward & forward as the integer 1 & it's inverse (1, -1) 
+
+// MARK: Position of the tile on the board - Struct
+// TODO: x & y pos variables
+// TODO: function for finding the previous position based on switch statement for orientation
+
+// MARK: Style configuration of the game - Struct
+// TODO: set tile foreground & background styles in arrays & manipulate them through functions
+// TODO: initialize the style of an empty board background & foreground colors
+
+// MARK: Size configuration of the board - Struct 
+// TODO: set tile number, tile count, board size, tile size, border size
+
+// MARK: Tile Class
+
+// MARK: Board Class
+
+// MARK: GameViewController Class
+
+
+
+
