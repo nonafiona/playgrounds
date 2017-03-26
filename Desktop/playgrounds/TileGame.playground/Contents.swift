2da5ac658b92a670fabbe7ce7b2d587f5ea99ead
fiona <fi.carty@gmail.com>
2017-03-26 15:47:36 -0700

    finished the Board class onto the GameViewController class

--- Desktop/playgrounds/TileGame.playground/Contents.swift
+++ Desktop/playgrounds/TileGame.playground/Contents.swift
@@ -215,12 +215,135 @@ class Board {
         return pointAt(x: position.x, y: position.y)
     }
     
+    func addTo(view: UIView) {
+        view.addSubview(self.boardView)
+        boardView.widthAnchor.constraint(equalToConstant: config.boardSize.width).isActive = true
+        boardView.heightAnchor.constraint(equalTo: boardView.widthAnchor).isActive = true
+        boardView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        boardView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
     
+    func gameOver() {
+        print("game over")
+    }
+    
+    func add(tile: Tile) {
+        tile.addTo(board: self)
+        tileArray.append(tile)
+    }
+    
+    func generateTile() {
+        guard tileArray.count <= config.tileCount else {
+            print("no space available")
+            return
+        }
+        
+        var tileList : [(Int,Int)?] = Array(repeating: nil, count: config.tileCount)
+        for x in 0..<config.tileNumber {
+            for y in 0..<config.tileNumber {
+                tileList[x+y*config.tileNumber] = (x,y)
+            }
+        }
+        for tile in tileArray {
+            tileList[tile.position.x + tile.position.y * config.tileNumber] = nil
+        }
+        
+        let remain = tileList.flatMap {$0}
+        let random = arc4random_uniform(UInt32(remain.count))
+        let value = Int(arc4random_uniform(3)/2)+1
+        let (x,y) = remain[Int(random)]
+        let tile = Tile(value: value, position: Position(x: x, y: y))
+        
+        tile.addTo(board: self)
+        tileArray.append(tile)
+    }
+    
+    func buildBoard() {
+        for i in 0..<4 {
+            for j in 0..<4{
+                let layer = CALayer()
+                layer.frame = CGRect(origin: pointAt(x: i, y: j), size: config.tileSize)
+                layer.backgroundColor = style.emptyTileBackgroundColor.cgColor
+                layer.cornerRadius = 3
+                boardView.layer.addSublayer(layer)
+            }
+        }
+        generateTile()
+        generateTile()
+    }
+    
+    func removeFromTileArray(tile: Tile) {
+        if let idx = tileArray.index(where: {$0 == tile}) {
+            tileArray.remove(at: idx)
+            tile.removeFromBoard()
+        }
+    }
+    
+    func checkMovement(direction: Direction, orientation:Orientation) -> Bool {
+        var moved = false
+        var tileList = [Tile]()
+        for y in 0..<config.tileNumber {
+            for x in 0..<config.tileNumber {
+                let tile = Tile(value: 0, position: Position(x: x, y: y))
+                tileList.append(tile)
+                
+            }
+        }
+        
+        for tile in tileArray {
+            tileList[tile.position.x + tile.position.y * config.tileNumber] = tile
+        }
+        var lastZeroTile: Tile? = nil
+        var lastMergeableTile: Tile? = nil
+        for i in 0..<config.tileNumber {
+            lastZeroTile = nil
+            lastMergeableTile = nil
+            for j in 0..<config.tileNumber {
+                let temp = direction == .forward ? (config.tileNumber - 1) - j : j
+                let x = orientation == .horizontal ? temp : i
+                let y = orientation == .horizontal ? i : temp
+                let tile = tileList[x + y * config.tileNumber]
+                if !tile.isEmpty {
+                    if let mergableTile = lastMergeableTile, mergableTile.value == tile.value {
+                        tile.mergeTo(position: mergableTile.position)
+                        removeFromTileArray(tile: mergableTile)
+                        lastMergeableTile = nil
+                        lastZeroTile = tile.createPreviousEmptyTile(direction: direction, orientation: orientation)
+                        moved = true
+                        continue
+                    }
+                    if let zeroTile = lastZeroTile {
+                        tile.moveTo(position: zeroTile.position)
+                        lastZeroTile = tile.createPreviousEmptyTile(direction: direction, orientation: orientation)
+                        moved = true
+                    }
+                    lastMergeableTile = tile
+                } else {
+                    if lastZeroTile == nil {
+                        lastZeroTile = tile
+                    }
+                }
+            }
+        }
+        return moved
+    }
+    
+    
+    func moveTile(direction: Direction, orientation: Orientation) {
+        let moved = checkMovement(direction: direction, orientation: orientation)
+        UIView.animate(withDuration: 0.1, animations: {
+            self.boardView.layoutIfNeeded()
+        }) { (_) in
+            if moved {
+                self.generateTile()
+            }
+        }
+    }
 }
 
-
-
 // MARK: GameViewController Class
+
+
 
 
 
