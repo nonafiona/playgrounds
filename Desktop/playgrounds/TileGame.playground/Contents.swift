cd2091881021339878c0a33726930fb41fb3fc7a
fiona <fi.carty@gmail.com>
2017-03-26 14:10:05 -0700

    added tile class & now working on the Board class

--- Desktop/playgrounds/TileGame.playground/Contents.swift
+++ Desktop/playgrounds/TileGame.playground/Contents.swift
@@ -73,12 +73,152 @@ struct Style {
     }
 }
 
-// MARK: Size configuration of the board - Struct 
+// MARK: Size configuration of the board - Struct
 // TODO: set tile number, tile count, board size, tile size, border size
+
+struct BoardSizeConfig {
+    
+    let tileNumber = 4
+    let tileCount = 16
+    let boardSize = CGSize(width: 290, height: 290)
+    let tileSize = CGSize(width: 60, height: 60)
+    let borderSize = CGSize(width: 10, height: 10)
+}
+
+// MARK: Instances 
+
+let style = Style()
+let config = BoardSizeConfig()
 
 // MARK: Tile Class
 
+class Tile: Equatable {
+    var value: Int = 0 {
+        didSet {
+            updateView()
+        }
+    }
+    var valueText : String {
+        return value == 0 ? "" : "\(1<<value)"
+    }
+    var valueLength : Int {
+        return valueText.characters.count
+    }
+    var isEmpty : Bool {
+        return value == 0
+    }
+    
+    let view : UILabel = UILabel(frame: .zero)
+    
+    var position : Position {
+        didSet {
+            guard let board = self.board else {
+                return
+            }
+            let point = board.pointAt(position: self.position)
+            self.topConstraint?.constant = point.y
+            self.leftConstraint?.constant = point.x
+        }
+    }
+    
+    var board : Board?
+    var topConstraint : NSLayoutConstraint?
+    var leftConstraint : NSLayoutConstraint?
+    
+    init(value: Int, position: Position = Position(x: 0, y: 0)) {
+        self.position = position
+        view.textAlignment = .center
+        view.layer.cornerRadius = 3
+        view.translatesAutoresizingMaskIntoConstraints = false
+        self.value = value
+        updateView()
+    }
+    
+    func fontSize(for length: Int) -> CGFloat {
+        if length > 4 {
+            return 18
+        } else if length > 3 {
+            return 20
+        }
+        return 30
+    }
+    
+    func updateView() {
+        view.text = valueText
+        view.font = UIFont.boldSystemFont(ofSize: fontSize(for: valueLength))
+        view.layer.backgroundColor = style.tileBackgroundColor(value: value)
+        view.textColor = style.tileForegroundColor(value: value)
+    }
+    
+    func moveTo(position: Position) {
+        self.position = position
+    }
+    
+    func mergeTo(position: Position) {
+        moveTo(position: position)
+        self.value += 1
+    }
+    
+    func addTo(board: Board) {
+        guard self.board == nil else {
+            return
+        }
+        self.board = board
+        let boardView = board.boardView
+        boardView.addSubview(view)
+        view.widthAnchor.constraint(equalToConstant: config.tileSize.width).isActive = true
+        view.heightAnchor.constraint(equalToConstant: config.tileSize.height).isActive = true
+        
+        let point = board.pointAt(position: self.position)
+        topConstraint = view.topAnchor.constraint(equalTo: boardView.topAnchor, constant: point.y)
+        leftConstraint = view.leftAnchor.constraint(equalTo: boardView.leftAnchor, constant: point.x)
+        topConstraint?.isActive = true
+        leftConstraint?.isActive = true
+    }
+    
+    func removeFromBoard() {
+        self.view.removeFromSuperview()
+    }
+    
+    func createPreviousEmptyTile(direction: Direction, orientation: Orientation) -> Tile {
+        let pos = self.position.previousPosition(direction: direction, orientation: orientation)
+        return Tile(value: 0, position: pos)
+    }
+}
+
+func ==(lhs: Tile, rhs: Tile) -> Bool {
+    return lhs.value == rhs.value && lhs.position == rhs.position
+}
+
 // MARK: Board Class
+
+class Board {
+    let boardView : UIView
+    var tileArray = [Tile]()
+    
+    init() {
+        boardView = UIView(frame: .zero)
+        boardView.backgroundColor = style.boardBackgroundColor
+        boardView.layer.cornerRadius = 6
+        boardView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func pointAt(x: Int, y: Int) -> CGPoint {
+        let offsetX = config.borderSize.width
+        let offsetY = config.borderSize.height
+        let width = config.tileSize.width + config.borderSize.width
+        let height = config.tileSize.height + config.borderSize.height
+        return CGPoint(x: offsetX + width * CGFloat(x), y: offsetY + height * CGFloat(y))
+    }
+    
+    func pointAt(position: Position) -> CGPoint {
+        return pointAt(x: position.x, y: position.y)
+    }
+    
+    
+}
+
+
 
 // MARK: GameViewController Class
 
