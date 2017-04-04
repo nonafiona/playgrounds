029c6aa01c801b88246c5260ce2f740bb81d12c3
fiona <fi.carty@gmail.com>
2017-04-04 14:29:09 -0700

    added insertion function

--- Desktop/playgrounds/BinarySearchTree.playground/Contents.swift
+++ Desktop/playgrounds/BinarySearchTree.playground/Contents.swift
@@ -2,6 +2,8 @@
 
 // OOP Implementation
 
+// This class describes a single node, not the entire tree
+
 public class BinarySearchTree<T: Comparable> {
     
     private(set) public var value: T
@@ -48,7 +50,28 @@ public class BinarySearchTree<T: Comparable> {
     public var count: Int {
         return (left?.count ?? 0) + 1 + (right?.count ?? 0)
     }
+    
+    // inserting nodes method
+    
+    public func insert(value: T) {
+        if value < self.value {
+            if let left = left {
+                left.insert(value: value)
+            } else {
+                left = BinarySearchTree(value: value)
+                left?.parent = self
+            }
+        }else{
+            if let right = right {
+                right.insert(value: value)
+            } else {
+                right = BinarySearchTree(value: value)
+                right?.parent = self
+            }
+        }
+    }
 }
+    
 
 
 
