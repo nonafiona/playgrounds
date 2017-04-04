9ec7ce90d7dd2e22fcfbf2d197ab3c32646f832f
fiona <fi.carty@gmail.com>
2017-04-04 14:08:33 -0700

    started OOP implementation of Binary Search Tree :~) 🎷🐛

---
+++ Desktop/playgrounds/BinarySearchTree.playground/Contents.swift
@@ -0,0 +1,92 @@
+// Binary Search Tree
+
+// OOP Implementation
+
+public class BinarySearchTree<T: Comparable> {
+    
+    private(set) public var value: T
+    private(set) public var parent: BinarySearchTree?
+    private(set) public var left: BinarySearchTree?
+    private(set) public var right: BinarySearchTree?
+    
+    public init(value: T) {
+        self.value = value
+    }
+    
+    public var isRoot: Bool {
+        return parent == nil
+    }
+    
+    public var isLeaf: Bool {
+        return left == nil && right == nil
+    }
+    
+    public var isLeftChild: Bool {
+        return parent?.left === self
+    }
+    
+    public var isRightChild: Bool {
+        return parent?.right === self
+    }
+    
+    public var hasLeftChild: Bool {
+        return left != nil
+    }
+    
+    public var hasRightChild: Bool {
+        return right != nil
+    }
+    
+    public var hasAnyChild: Bool {
+        return hasLeftChild || hasRightChild
+    }
+    
+    public var hasBothChildren: Bool {
+        return hasLeftChild && hasRightChild
+    }
+    
+    public var count: Int {
+        return (left?.count ?? 0) + 1 + (right?.count ?? 0)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Enum Implementation
+
